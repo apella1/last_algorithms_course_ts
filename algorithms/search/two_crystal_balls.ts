5b3c791a0b270b676578224ba305efd335493b42
apella1 <johncollons5@gmail.com>
2024-02-21 12:14:54 +0300

    algorithm(problem): two crystal balls problem

---
+++ algorithms/search/two_crystal_balls.ts
@@ -0,0 +1,21 @@
+// Optimization problem known as "Two Crystal Balls Problem" or "Two Egg Problem"
+function twoCrystalBalls(breaks: boolean[]): number {
+  const jmpAmount = Math.floor(Math.sqrt(breaks.length));
+  let i = jmpAmount;
+
+  for (; i < breaks.length; i += jmpAmount) {
+    if (breaks[i]) {
+      break;
+    }
+  }
+
+  i -= jmpAmount;
+
+  for (let j = 0; j < jmpAmount && i < breaks.length; ++j, ++i) {
+    if (breaks[i]) {
+      return i;
+    }
+  }
+
+  return -1;
+}
